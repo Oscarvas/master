511cb48612bec55e3663f314f1092b636d6ad8d3
Oscar Vasquez <oscardavid.vp@gmail.com>
2022-05-12 00:38:11 +0200

    ejerccio A3

--- Z - Codigo/enunciados.py
+++ Z - Codigo/enunciados.py
@@ -4,3 +4,9 @@ def redondeo_hora(str: string):
     return str.split(':')[0]
 
 print(redondeo_hora('12:48'))
+
+def lesividad(str: string):
+    return int(str) if str else 0
+
+for c in  ['01', '02', '14', '', '77']:
+    print(c, " -> ", lesividad(c))
