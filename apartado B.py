37b64f9df2ee28572166feb892628d301c8c4ef5
Oscar Vasquez <oscardavid.vp@gmail.com>
2022-05-14 20:19:26 +0200

    Apartado b1

--- apartado B.py
+++ apartado B.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 def cargar_cabecera(fichero: str):
-    pd.read_csv('../'+fichero, encoding='latin1')[0]
+   df = pd.read_csv(fichero, encoding='iso-8859-1', nrows=0, sep=';')
+   # df.rename(columns={'Unnamed: 13':'', 'Unnamed: 14':''},inplace=True)
+   # la opcion anterior para columnas fijas. 
+   # la nueva para cualquiera (list comprehension)
+
+   return ['' if 'Unname' in col else col for col in df.columns ]
             
 cabecera = cargar_cabecera("2020_Accidentalidad.csv")
 print(cabecera)
